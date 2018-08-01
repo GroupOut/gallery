4269c2a386e3f01ab6492d2d0306b7dfc1744c1e
Christian Noh <christiannoh@gmail.com>
2018-07-31 17:21:17 -0700

    Implements fixes for the proxy

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const db = require('./query.js');
 const app = express();
 const filepath = path.join(__dirname, './client/dist');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(filepath));
@@ -16,6 +21,10 @@ app.get('/images/:id', (req, res) => {
     if (error) throw error;
     res.send({ data: images });
   });
+});
+
+app.get('/app.js', (req, res) => {
+  res.sendFile(`${filepath}/bundle.js`);
 });
 
 app.listen(3003, () => console.log('Listening on port 3003!'));

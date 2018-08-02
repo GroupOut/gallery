8da034eb29dd78548c89428d97eb117f6fc3793b
Christian Noh <christiannoh@gmail.com>
2018-08-02 10:36:46 -0700

    Navigation bar skeleton

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Gallery from './components/Gallery.jsx';
+import Navbar from './components/Navbar.jsx';
 
 ReactDOM.render(<Gallery />, document.getElementById('gallery'));
+ReactDOM.render(<Navbar />, document.getElementById('navbar'));
